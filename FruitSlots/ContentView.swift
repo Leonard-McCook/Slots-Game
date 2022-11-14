aadee4fe1106e1f7bd9458a23c514db32a7df00e
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-14 11:42:46 -0500

    Initial Commit

---
+++ FruitSlots/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  FruitSlots
+//
+//  Created by Leonard McCook-Carr on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var credits = 1000
+    @State var slot1 = 1
+    @State var slot2 = 1
+    @State var slot3 = 1
+    
+    var body: some View {
+        
+        VStack(spacing: 20.0) {
+            Spacer()
+            Text("Fruit Slots").font(.largeTitle)
+            Spacer()
+            
+            
+            Text("Credits \(credits)")
+            
+            HStack {
+                
+                Image("fruit\(slot1)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                   
+                   
+                
+                Image("fruit\(slot2)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    
+                Image("fruit\(slot3)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    
+            }
+            Spacer()
+            Button("Spin") {
+                
+               
+                slot1 = Int.random(in: 1...3)
+                slot2 = Int.random(in: 1...3)
+                slot3 = Int.random(in: 1...3)
+                
+                
+                if slot1 == slot2 && slot2 == slot3 {
+                    
+                    credits += 15
+                }
+                else {
+                    credits -= 5
+                }
+            }
+            
+            .padding()
+            
+            .padding([.leading, .trailing], 40)
+            .foregroundColor(.white)
+            .background(Color(.systemPink))
+            .cornerRadius(25)
+            .font(.system(size: 18, weight: .bold, design: .default))
+            Spacer()
+        }
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitSlots/FruitSlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSlotsApp.swift
+//  FruitSlots
+//
+//  Created by Leonard McCook-Carr on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitSlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
